4bfdecf40b461bbebabd44202a07cd14bb54b007
Khadijat Oyeleye <khadijat@try.be>
2022-08-04 11:46:30 +0100

    feat: created song controller

--- app/Http/Controllers/SongsController.php
+++ app/Http/Controllers/SongsController.php
@@ -2,9 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Song;
 use Illuminate\Http\Request;
 
 class SongsController extends Controller
 {
-    //
+    public function __invoke()
+    {
+        return Song::all();
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HelloController;
+use App\Http\Controllers\SongsController;
 
 /*
 |--------------------------------------------------------------------------
@@ -20,3 +21,5 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 Route::get('/hello', HelloController::class);
+
+Route::get('/songs', SongsController::class);
