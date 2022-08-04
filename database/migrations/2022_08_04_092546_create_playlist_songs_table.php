614b0f77b5c396fe7c3e8a632337609266e10451
Khadijat Oyeleye <khadijat@try.be>
2022-08-04 10:33:38 +0100

    initial commit

---
+++ database/migrations/2022_08_04_092546_create_playlist_songs_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('playlist_songs', function (Blueprint $table) {
+            $table->string('song_id');
+            $table->string('playlist_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('playlist_songs');
+    }
+};
